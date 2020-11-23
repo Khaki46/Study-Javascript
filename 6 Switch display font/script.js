7fc93b5c639f13d44c2fd8cbf37a7eaaa0cb46fa
Khaki46 <caibobo774@gmail.com>
2020-11-23 18:29:36 +0800

    Add

--- 6 Switch display font/script.js
+++ 6 Switch display font/script.js
@@ -1,1 +1,15 @@
-var liList=document.getElementsByTagName("li");
+        var content=document.getElementById("content");
+        var liList=content.getElementsByTagName("li");
+
+        var box=document.getElementById("box");
+        var divList=box.getElementsByTagName("div");
+        for(var i=0;i<liList.length;i++){
+            liList[i].index=i;
+            liList[i].onclick = function(){
+                // document.divList[this.index].sty
+                for(var a=0;a<liList.length;a++){
+                    divList[a].style.display="none";
+                }
+                divList[this.index].style.display="inline";
+            }
+        }
