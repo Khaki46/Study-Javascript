9ad7a79df8a4666307bc5e02a44ba5f9c7d8e110
Khaki46 <ikourou8@gmail.com>
2020-11-29 00:35:22 +0800

    Add

--- 8 Drag your ball/script.js
+++ 8 Drag your ball/script.js
@@ -2,13 +2,16 @@ var ball = document.getElementById("ball");
 
 ball.onmousedown = function(ev){
     var ev = ev || Event;
-
+    // ev.clientX为鼠标点击球时鼠标到屏幕边的距离
+    // this.offsetLeft为球边缘到屏幕边的距离
+    // left为鼠标到球边的距离
     var top = ev.clientY - this.offsetTop;
     var left = ev.clientX - this.offsetLeft;
-
+    // console.log(ev.clientX,ev.clientY);
+    console.log(this.offsetLeft,this.offsetTop);
     document.onmousemove = function(ev){
         var ev = ev || Event;
-        console.log(ev.clientX,ev.clientY);
+        // console.log(ev.clientX,ev.clientY);
 
         ball.style.left = ev.clientX - left + 'px';
         ball.style.top = ev.clientY - top + 'px';
---
+++ 9 Pull your cube/script.js
@@ -0,0 +1,10 @@
+var box = document.getElementById("box");
+
+box.onmousedown = function(){
+    var ev = ev || Event;
+
+    var left = ev.clientX;
+    var top = ev.clientY;
+
+
+}
