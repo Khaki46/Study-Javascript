6cd42adf5ed0b8b9060c20d7ac08e7b34102432f
Khaki46 <ikourou8@gmail.com>
2020-11-29 14:00:45 +0800

    Add

--- 9 Pull your cube/script.js
+++ 9 Pull your cube/script.js
@@ -1,10 +1,22 @@
 var box = document.getElementById("box");
 
-box.onmousedown = function(){
-    var ev = ev || Event;
+box.onmousedown = function(ev){
+    var ev = ev || event;
 
     var left = ev.clientX;
     var top = ev.clientY;
 
+    var width = this.offsetWidth;
+    var height = this.offsetHeight;
 
-}
+    console.log(top,left);
+    document.onmousemove = function(ev){
+        var ev = ev || event;
+        box.style.width = ev.clientX - left + width +'px';
+        box.style.height = ev.clientY - top + height + 'px';
+        
+    };
+    box.onmouseup = function(){
+        document.onmouseup = document.onmousemove = null;
+    };
+};
