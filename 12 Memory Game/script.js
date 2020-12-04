231cf26e08a241860020ca6217755edd82e1d44d
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2020-12-05 00:01:03 +0800

    Fix bug

--- 12 Memory Game/script.js
+++ 12 Memory Game/script.js
@@ -50,38 +50,46 @@ const imgArr=[
 ]
 imgArr.sort(function(){return 0.5-Math.random();});
 const img = document.getElementsByTagName('img');
+let switchTF ;
 let cardId = [];
 let cardArr = [];
+let imgIdArr = [];
+function detect(){
+    var cards = document.querySelectorAll('img');
+    const cardArrOne = imgIdArr[0];
+    const cardArrTwo = imgIdArr[1];
+    console.log(cardArrOne);
+    console.log(cardArrTwo);
+    if(cardArrOne==cardArrTwo){
+        cardArr[0].setAttribute('src','img/close.png');
+        cardArr[1].setAttribute('src','img/close.png');
+    }else if(cardId[0]===cardId[1]){
+        cardArr[0].setAttribute('src','img/open.png');
+        cardArr[1].setAttribute('src','img/open.png');
+        cardArr[0].removeEventListener('click', test)
+        cardArr[1].removeEventListener('click', test)
+        }else{
+        cardArr[0].setAttribute('src','img/close.png');
+        cardArr[1].setAttribute('src','img/close.png');
+    }
+    // 重置
+    cardArr=[]
+    cardId=[]
+    imgIdArr=[]
+}
 for (let i = 0; i < img.length; i++) {
     console.log(imgArr[i]);
     img[i].addEventListener('click',flipCard);
     function flipCard(){
+        
         img[i].setAttribute('src',imgArr[i].img)
-        img[i].setAttribute('index',i);
-        let carId = img[i].getAttribute('index');
+        img[i].index=i;
+        imgIdArr.push(this.index);
         cardArr.push(img[i]);
         cardId.push(imgArr[i].name);
         if(cardId.length===2){
-            setTimeout(detect, 100);
+            setTimeout(detect, 300);
         }
     }
-
 }
-function detect(){
-    let cards = document.querySelectorAll('img');
 
-    const cardArrOne = cardId[0];
-    const cardArrTwo = cardId[1];
-    console.log(cardId[0]);
-    console.log(cardId[1]);
-    if(cardId[0]===cardId[1]){
-        cardArr[0].setAttribute('src','img/open.png');
-        cardArr[1].setAttribute('src','img/open.png');
-        cardArr[0].removeEventListener('click',flipCard);
-        cardArr[1].removeEventListener('click',flipCard);
-        console.log(cards[0]);
-    }else{
-        cardArr[0].setAttribute('src','img/close.png');
-        cardArr[1].setAttribute('src','img/close.png');
-    }
-}
