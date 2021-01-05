9a8b61a7c9968f5f89000ff484dfc48357a62767
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-01-05 12:16:26 +0800

    Finish

--- 13 Dino Runner/script.js
+++ 13 Dino Runner/script.js
@@ -1,13 +1,18 @@
-// document.addEventListener('DOMContentLoaded',() =>{
+document.addEventListener('DOMContentLoaded',() =>{
  
 const dino = document.getElementById('dino');
 const dinoImg = dino.querySelector('img');
 const cactus = document.getElementById('cactus');
 const cactusImg = cactus.querySelector('img');
+const bird = document.getElementById('bird');
+const birdImg = bird.querySelector('img');
 
+let Score = document.getElementsByTagName('span')[0];
+let scoreNum=0;
+let dinoDownTF=false;
 let dinoTF=true;
 function jump() {
-
+    
     if(dino.classList!='jump'){
         dino.classList.add("jump");
         setTimeout(() => {
@@ -17,12 +22,12 @@ function jump() {
 }
 function down() {
         dinoTF=false;
+        dinoDownTF=true;
         document.addEventListener('keyup',control1);
         function control1(){
         dinoTF=true;
+        dinoDownTF=false;
         dinoImg.setAttribute('src','img/dino.png')
-
-
     }
 }
 function control(e) {
@@ -36,16 +41,25 @@ function control(e) {
     }
 
 }
-
+setInterval(() => {
+    scoreNum++;
+    Score.innerHTML=scoreNum;
+}, 1000);
 
 let dinoAlive = setInterval(() => {
     // 获得恐龙高度距离
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     // 获得仙人掌宽度距离
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    // 获得鸟的宽度距离
+    let birdLeft = parseInt(window.getComputedStyle(bird).getPropertyValue("left"));
     if(cactusLeft<45 && cactusLeft>0 && dinoTop>=100){
-        // console.log("good");
         alert('you die!');
+        window.location.reload(); 
+    }
+    if(birdLeft<45 && birdLeft>0 && dinoDownTF==false){
+        alert('you die!');
+        window.location.reload(); 
     }
     if(dinoTop<140){
         dinoImg.setAttribute('src','img/dino.png');
@@ -71,6 +85,13 @@ function dinoGoFun2(){
 
     let dinoGo1=setInterval(dinoGoFun1, 100);
     let dinoGo2=setInterval(dinoGoFun2, 200);
+// 鸟飞翔动作
+setInterval(() => {
+    birdImg.setAttribute('src','img/bird1.png');
+}, 100);
+setInterval(() => {
+    birdImg.setAttribute('src','img/bird2.png');
+}, 250);
 // 更换仙人掌图片
 setInterval(() => {
     let num=Math.floor(Math.random()*10); 
@@ -80,9 +101,18 @@ setInterval(() => {
     }else if(3<num<=6){
         cactusImg.setAttribute('src','img/cactus3.png')
     }else if(6<num<=10){
-
         cactusImg.setAttribute('src','img/cactus4.png')
-
     }
 }, 2000);
-// })
+// 鸟和仙人掌出现
+setInterval(() => {
+    let ShowTime = Math.floor(Math.random()*10); 
+    if(ShowTime<5){
+        cactus.classList.add("cactusShow");
+        bird.classList.remove("birdShow");
+    }else if(ShowTime>=5){
+        bird.classList.add("birdShow");
+        cactus.classList.remove("cactusShow");
+    }
+}, 2000);
+})
