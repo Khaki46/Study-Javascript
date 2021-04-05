3fbdb66fa9dda47cb1f7d828cec5e328913ff543
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-04-05 22:05:14 +0800

    Add

--- 2048/script.js
+++ 2048/script.js
@@ -19,3 +19,26 @@ function getRandomText() {
     }
 }
 
+function moveright() {
+    for (let index = 0; index < 16; index++) {
+        // console.log(index+":"+index % 4);
+        if (index % 4 == 0) {
+            let horizontalOne = numArr[index].innerHTML;
+            let horizontalTwo = numArr[index+1].innerHTML;
+            let horizontalThree = numArr[index+2].innerHTML;
+            let horizontalFour = numArr[index+3].innerHTML;
+            // 转换为数字
+            let row = [parseInt(horizontalOne), parseInt(horizontalTwo), parseInt(horizontalThree), parseInt(horizontalFour)];
+            // 将数字都移动到最右边
+            let filterRow = row.filter(num => num);
+            let missing = 4 - filterRow.length;
+            let zeros = Array(missing).fill(0);
+            let newRow = zeros.concat(filterRow);
+            numArr[index].innerHTML = newRow[0];
+            numArr[index+1].innerHTML = newRow[1];
+            numArr[index+2].innerHTML = newRow[2];
+            numArr[index+3].innerHTML = newRow[3];
+        }
+    }
+}
+moveright();
