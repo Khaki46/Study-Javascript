abcfe53e573d748af2fb63d5588b25169b27138f
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-04-07 22:53:40 +0800

    Add change color and check lost

--- 2048/script.js
+++ 2048/script.js
@@ -2,7 +2,8 @@ let td = document.querySelectorAll('td');
 const widthNum=4;
 let numArr = [];
 let score = 0;
-function  createBoard() {
+let checkMove = true;
+function createBoard() {
     for (const iterator of td) {
         iterator.innerHTML = 0;
         numArr.push(iterator);
@@ -18,6 +19,7 @@ function getRandomText() {
     let random = Math.floor(Math.random() * (max - min + 1)) + min;
     if (td[random].innerHTML == 0) {
         td[random].innerHTML = 2;
+        checkForGameOver();
     } else {
         getRandomText();
     }
@@ -106,26 +108,30 @@ function moveBottom() {
 }
 function combineRow() {
     for (let i =0; i < 15; i++) {
-      if (numArr[i].innerHTML === numArr[i +1].innerHTML) {
-        let combinedTotal = parseInt(numArr[i].innerHTML) + parseInt(numArr[i +1].innerHTML)
-        numArr[i].innerHTML = combinedTotal
-        numArr[i +1].innerHTML = 0
-        score += combinedTotal
-        // scoreDisplay.innerHTML = score
-      }
+        if (numArr[i].innerHTML === numArr[i + 1].innerHTML) {
+            let combinedTotal = parseInt(numArr[i].innerHTML) + parseInt(numArr[i + 1].innerHTML)
+            numArr[i].innerHTML = combinedTotal  
+            numArr[i + 1].innerHTML = 0      
+            score += combinedTotal
+            checkMove = true;
+        } else {
+            checkMove = false;
+        }
     }
 }
 function combineColumn() {
     for (let i =0; i < 12; i++) {
-      if (numArr[i].innerHTML === numArr[i +widthNum].innerHTML) {
-        let combinedTotal = parseInt(numArr[i].innerHTML) + parseInt(numArr[i +widthNum].innerHTML)
-        numArr[i].innerHTML = combinedTotal
-        numArr[i +widthNum].innerHTML = 0
-        score += combinedTotal
-        // scoreDisplay.innerHTML = score
-      }
-    }
-  }
+        if (numArr[i].innerHTML === numArr[i +widthNum].innerHTML) {
+            let combinedTotal = parseInt(numArr[i].innerHTML) + parseInt(numArr[i +widthNum].innerHTML)
+            numArr[i].innerHTML = combinedTotal
+            numArr[i +widthNum].innerHTML = 0
+            score += combinedTotal
+            checkMove = true;
+        } else {
+            checkMove = false;
+        }
+        }
+}
 function keyOn(e) {
     if (e.key === "ArrowRight") {
         moveRight();
@@ -161,12 +167,74 @@ function youWin() {
 }
 document.addEventListener('keyup', youWin);
 
-document.onkeydown=function(e){
-		if(e.key == "ArrowUp" || e.key == "ArrowDown"){ 
+function checkForGameOver() {
+    let zeros = 0
+    for (let i=0; i < numArr.length; i++) {
+      if (numArr[i].innerHTML == 0) {
+          zeros++
+          console.log(zeros);
+      }
+    }
+    console.log("check:"+checkMove);
+    // console.log(zeros === 0);
+    if ((zeros === 0) && checkMove) {
+        alert("You lost");
+        document.removeEventListener('keyup', control)
+        setTimeout(() => clear(), 3000)
+    }
+}
+  
+document.onkeydown = function (e) {
+        changeColor();
+		if(e.key == "ArrowUp" || e.key == "ArrowDown"){
 		if(e.preventDefault){
-			e.preventDefault();
-		}else{
+            e.preventDefault();
+        } else {
 			e.returnValue = false;
 		}
 	}
 }
+function changeColor() {
+    for (let index = 0; index < 16; index++) {
+        if (numArr[index].innerHTML == 0) {
+            td[index].className = "";
+        }
+        if (numArr[index].innerHTML == 2) {
+            td[index].className = "td2";
+        }
+        if (numArr[index].innerHTML == 4) {
+            td[index].className = "td4";
+        }
+        if (numArr[index].innerHTML == 8) {
+            td[index].className = "td8";
+        }
+        if (numArr[index].innerHTML == 16) {
+            td[index].className = "td16";
+        }
+        if (numArr[index].innerHTML == 32) {
+            td[index].className = "td32";
+        }
+        if (numArr[index].innerHTML == 64) {
+            td[index].className = "td64";
+        }
+        if (numArr[index].innerHTML == 128) {
+            td[index].className = "td128";
+        }
+        if (numArr[index].innerHTML == 256) {
+            td[index].className = "td256";
+        }
+        if (numArr[index].innerHTML == 512) {
+            td[index].className = "td512";
+        }
+        if (numArr[index].innerHTML == 1024) {
+            td[index].className = "td1024";
+        }
+        if (numArr[index].innerHTML == 2048) {
+            td[index].className = "td2048";
+        }
+        if (numArr[index].innerHTML == 4096) {
+            td[index].className = "td4096";
+        }
+    }
+}
+changeColor();
